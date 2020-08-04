cf8bdfafed0276704ca3f2d2717a1bbb22e596d3
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-08-04 16:12:14 -0500

    Setup the function to add movies to the api, but need to refactor our code for posting and getting the movie information onto our page.

--- src/api.js
+++ src/api.js
@@ -1,12 +1,33 @@
 const $ = require("jquery");
 
+const movies = fetch('/api/movies').then(response => {
+    return response.json();
+});
+
 module.exports = {
   getMovies: () => {
-    const subtitle = $(".container h3").html("Loading...");
-    return fetch('/api/movies')
-      .then(response => {
-        subtitle.html("");
-        return response.json();
-      });
-  }
+      // Set loading placeholder
+    // $(".container h3").html("Loading...");
+    return movies;
+  },
+    // function to add a new movie
+    addMovie: (title, rating, length) => {
+        const movie = {
+            title: title,
+            rating: rating,
+            id: length + 1
+        };
+
+        const url = '/api/movies';
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(movie)
+        };
+        fetch(url, options)
+            .then((data) => console.log(data))
+            .catch(/* handle errors */);
+    }
 };
--- src/index.js
+++ src/index.js
@@ -7,20 +7,31 @@ import sayHello from './hello';
 sayHello('World');
 
 const $ = require("jquery");
+const formCreate = $("#form-create");
 
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
+const {getMovies, addMovie} = require('./api.js');
 
+// * May need to make this a render function *
 getMovies().then((movies) => {
   const para = $(".col p");
-  para.html("");
+  $(".container h3").html("");
   movies.forEach(({title, rating, id}) => {
     para.append(`<div id=${id}>id#${id} - ${title} - rating: ${rating}</div>`);
   });
-
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+// Event Listener for Add Movie Button
+formCreate.on("submit", (e) => {
+  e.preventDefault();
+  const newMovie = $("#movieTitle").val();
+  const rating = $("#movieRating").val();
+  getMovies().then((data) => {
+    addMovie(newMovie, rating, data.length);
+  });
+})
