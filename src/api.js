dfe7f7fb607d5920990e537e3d4d6897b42a0c68
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-08-06 02:53:35 -0500

    Added in the edit feature and can edit/modify a movie within db.json, but may need to add in a few additional default features for the edit tab section. Still need to add in the delete feature and eventually work on fixing up the design layout.

--- src/api.js
+++ src/api.js
@@ -1,45 +1,58 @@
 const $ = require("jquery");
 
 const movies = (obj) => {
-    $(".col p").html("Loading...");
+    $("#movieList").html("Loading...");
     if (Object.keys(obj).length < 2) {
         return fetch(obj.url).then(response => {
             return response.json();
         })
-    }
-    else {
+    } else {
         return fetch(obj.url, obj.options).then(response => {
             return response.json();
         })
     }
 }
 
-module.exports = {
-  getMovies: () => {
-      // Set loading placeholder
+// module.exports = {
+const getMovies = () => {
+    // Set loading placeholder
     // $(".container h3").html("Loading...");
     return movies({
         url: '/api/movies'
     });
-  },
-    // function to add a new movie
-    addMovie: (title, rating, length) => {
-        const movie = {
-            title: title,
-            rating: rating,
-            id: length + 1
-        };
-        //console.log(Object.keys(movie).length);
-        const url = '/api/movies';
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(movie)
-        };
-        return movies({url, options})
-            //.then((data) => (data))
-            //.catch(/* handle errors */);
+}
+
+// function to add a new movie
+const addMovie = (title, rating, length) => {
+    const movie = {
+        title: title,
+        rating: rating,
+        id: length + 1
+    };
+    //console.log(Object.keys(movie).length);
+    const url = '/api/movies';
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(movie)
+    };
+    return movies({url, options})
+}
+
+// function to edit a movie
+const editMovie = (id, editMovie) => {
+    const url = `/api/movies/${id}`;
+    const options = {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(editMovie)
     }
-};
+    return movies({url, options})
+}
+// };
+
+export { getMovies, addMovie, editMovie };
--- src/index.js
+++ src/index.js
@@ -4,61 +4,109 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import sayHello from './hello';
-sayHello('World');
-
 const $ = require("jquery");
-const formCreate = $("#form-create");
-const formEdit = $("#form-edit");
-const editTab = $("#edit-tab");
-const createTab = $("#create-tab");
+
+sayHello('World');
 
 /**
  * require style imports
  */
-const {getMovies, addMovie} = require('./api.js');
+// const {getMovies, addMovie, editMovie} = require('./api.js');
+import { getMovies, addMovie, editMovie } from "./api";
+
+const formCreate = $("#form-create");
+const formEdit = $("#form-edit");
+const createTab = $("#create-tab");
+const editTab = $("#edit-tab");
+const editTitle = $("#movieTitleEdit");
+const editRating = $("#movieRatingEdit");
 const movieList = $("#movieList");
+let radioBtns = {};
+let editID = '';
 
 
-// * May need to make this a render function *
-const renderMovies = () => {
-  getMovies().then((movies) => {
-    $(".container p").html("");
+const render = () => getMovies().then((movies) => {
+    movieList.html("");
     movies.forEach(({title, rating, id}) => {
-      movieList.append(`<div id=${id}>${title} - rating: ${rating} <button class="d-none" id=deleteBtn${id}>Delete</button></div>`);
+        movieList.append(
+            `<div class="d-flex align-items-baseline">` +
+                `<input type="radio" id="selector${id}" name="selected-movie" value="movie${id}" class="d-none">` +
+                `<label for="selector${id}" id="lbl${id}" class="col-10">` +
+                    `<div class="d-flex">` +
+                        `<div class="col-5"><div><span class="mTitle">${title}</span></div><div>rating: <span class="mRating">${rating}</span></div></div>` +
+                        `<div><button class="d-none" id="deleteBtn${id}">Delete</button></div>` +
+                    `</div>` +
+                `</label>` +
+            `</div>`);
     });
-  }).catch((error) => {
+    radioBtns = $("#movieList input");
+}).catch((error) => {
     alert('Oh no! Something went wrong.\nCheck the console for details.')
     console.log(error);
-  });
+});
+
+// Function to render movies to the page
+const renderMovies = () => {
+    render();
 }
+
+// Function to handle the edit form section
+const handleEditForm = () => {
+    movieList.children().each(function (index) {
+        $(`#deleteBtn${index + 1}`).removeClass('d-none');
+        $(`#selector${index + 1}`).removeClass('d-none');
+    });
+
+    // Event Listener for Radio Buttons when selected
+    radioBtns.click(() => {
+        $.each(radioBtns, function (idx) {
+            if ($(`#movieList #selector${idx + 1}`).prop("checked")) {
+                const movieTitle = $(`#lbl${idx + 1} .mTitle`).text();
+                const movieRating = $(`#lbl${idx + 1} .mRating`).text();
+                editTitle.val(movieTitle);
+                editRating.val(movieRating);
+                editID = idx + 1;
+            }
+        });
+    });
+}
+
 renderMovies();
 
-// Event Listener for Add Movie Button
+// Event Listener for Create Tab
+createTab.click(() => {
+    movieList.children().each(function (index) {
+        $(`#deleteBtn${index + 1}`).addClass('d-none');
+        $(`#selector${index + 1}`).addClass('d-none');
+    });
+});
+
+//Event Listener for Edit Movie Tab
+editTab.click(() => {
+    handleEditForm();
+});
+
+// Event Listener for Create Movie form
 formCreate.on("submit", (e) => {
-  e.preventDefault();
-  const newMovie = $("#movieTitleAdd").val();
-  const rating = $("#movieRatingAdd").val();
-  getMovies().then((data) => {
-    movieList.html("");
-    addMovie(newMovie, rating, data.length)
-        .then(renderMovies);
-  });
-})
+    e.preventDefault();
+    const newMovie = $("#movieTitleAdd").val();
+    const rating = $("#movieRatingAdd").val();
+    getMovies().then((data) => {
+        movieList.html("");
+        addMovie(newMovie, rating, data.length)
+            .then(renderMovies);
+    });
+});
 
-//Event Listener for Edit Movie Button
-editTab.click(function() {
-  //console.log($(this)); calls editTab
-  console.log(movieList);
-  movieList.children().each(function(index) {
-    $(`#deleteBtn${index + 1}`).removeClass('d-none');
-        console.log("Removed class");
-      })
-})
+// Event Listener for Edit Movie form
+formEdit.on("submit", (e) => {
+    e.preventDefault();
+    editMovie(editID, {
+        title: editTitle.val(),
+        rating: editRating.val()
+    }).then(() => {
+        render().then(handleEditForm);
+    });
+});
 
-createTab.click(function() {
 
-  movieList.children().each(function (index) {
-    $(`#deleteBtn${index + 1}`).addClass('d-none');
-    console.log("Added d-none");
-  })
-})
